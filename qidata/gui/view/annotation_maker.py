365ba33d8abdba60d09331ec89d0a97fbcf09144
sambrose <sambrose@aldebaran.com>
2016-05-09 17:23:24 +0200

    Remove useless buttons from annotation maker

--- qidata/gui/view/annotation_maker.py
+++ qidata/gui/view/annotation_maker.py
@@ -2,7 +2,7 @@
 
 from .editable_tree import EditableTree
 
-from PySide.QtGui import QWidget, QHBoxLayout, QVBoxLayout, QPushButton, QComboBox
+from PySide.QtGui import QWidget, QVBoxLayout, QComboBox
 from PySide.QtCore import Signal
 
 from qidata import annotationitems
@@ -36,32 +36,13 @@ class AnnotationMaker(QWidget):
         # Middle widget: annotation composition
         self.editable_tree = EditableTree(self)
 
-        # Bottom layer widget: Import data from another frame and save the current annotation
-        self.utils_widget = QWidget(self)
-
-        self.import_button = QPushButton("Import from...", self.utils_widget)
-        self.save_button = QPushButton("Save", self.utils_widget)
-
-        self.utils_hlayout = QHBoxLayout(self)
-        self.utils_hlayout.addWidget(self.import_button)
-        self.utils_hlayout.addWidget(self.save_button)
-        self.utils_widget.setLayout(self.utils_hlayout)
-
-
         # Integrate all the widgets and the top layer widget into the main layout
         self.main_vlayout = QVBoxLayout(self)
         self.main_vlayout.addWidget(self.definition_widget)
         self.main_vlayout.addWidget(self.editable_tree)
-        self.main_vlayout.addWidget(self.utils_widget)
         self.setLayout(self.main_vlayout)
 
         ## GUI ELEMENT SETUP
-
-        # Save button
-        self.save_button.setEnabled(True)
-
-        # Import button
-        self.import_button.setEnabled(True)
 
         # Topic selection
         self.annotation_type_selection_widget.addItems(annotationitems.AnnotationTypes) # we add some message type
