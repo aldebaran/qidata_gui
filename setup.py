2bd04fa0aa752df6b5e73c14a8e95f0769ca8bb4
sambrose <sambrose@aldebaran.com>
2016-05-09 14:11:02 +0200

    Fix install rules

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     version=open(os.path.join(CONTAINING_DIRECTORY,"qidata/VERSION")).read().split()[0],
     author='Louis-Kenzo Cahier',
     author_email='lkcahier@aldebaran.com',
-    packages=['qidata', 'qidata.commands', 'qidata.gui', 'qidata.models', 'qidata.qiq'],
+    packages=['qidata', 'qidata.commands', 'qidata.gui', 'qidata.annotationitems', 'qidata.qiq'],
     package_data={"qidata":["VERSION"]},
     scripts=['bin/qidata'],
     url='.',
