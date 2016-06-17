247168c424d97423df69a0fac16f30e67686b9cd
sambrose <sambrose@aldebaran.com>
2016-06-17 11:52:04 +0200

    Export all subpackages

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+from setuptools import setup, find_packages
 import os
 
 CONTAINING_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
@@ -11,7 +11,7 @@ setup(
     version=open(os.path.join(CONTAINING_DIRECTORY,"annotator/VERSION")).read().split()[0],
     author='Surya Ambrose',
     author_email='sambrose@aldebaran.com',
-    packages=['annotator', 'annotator.commands', 'annotator.gui', 'annotator.qiq'],
+    packages=find_packages("."),
     package_data={"annotator":["VERSION"]},
     scripts=['bin/annotator'],
     url='.',
