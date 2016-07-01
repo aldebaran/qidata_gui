f0695abb86e55bc0c1a6301dd3d417357029cbb4
sambrose <sambrose@aldebaran.com>
2016-07-01 13:58:12 +0200

    Re-organize widget factory API

--- qidata_widgets/__init__.py
+++ qidata_widgets/__init__.py
@@ -5,6 +5,7 @@ import os.path
 import re
 
 from image_widget import ImageWidget
+from qidatawidget import QiDataWidget
 from qidata_file import *
 
 
@@ -12,14 +13,23 @@ from qidata_file import *
 # Data Widgets
 
 LOOKUP_WIDGET_MODEL = {
-    Image: ImageWidget,
+    "image": ImageWidget,
 }
 
-def makeWidget(qidataFile):
-    for qidataFileType in LOOKUP_WIDGET_MODEL:
-        if isinstance(qidataFile, qidataFileType):
-            return LOOKUP_WIDGET_MODEL[qidataFileType](qidataFile)
-    raise TypeError("No available widget for this item")
+def makeWidget(widget_type, main_data=None):
+    """
+    Create relevant widget for required data type.
+    @widget_type  :  Name of the data type to display with the widget (str)
+    @main_data    :  Data to display
+    @return       :  Widget containing the given data (QiDataWidget)
+    @raise        :  TypeError if given type name is unknown
+    """
+    for qidata_file_type in LOOKUP_WIDGET_MODEL:
+        if widget_type == qidata_file_type:
+            return QiDataWidget(LOOKUP_WIDGET_MODEL[qidata_file_type](main_data))
+    raise TypeError("No available widget for %s, available types are %s"
+                        %(qidata_file_type, LOOKUP_WIDGET_MODEL.keys())
+                    )
 
 # ––––––––––––––––––––––––––––
 # Convenience version variable
