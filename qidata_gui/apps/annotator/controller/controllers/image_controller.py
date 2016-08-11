7ee4c85179eac0dfa44a0d07badeb83280518244
sambrose <sambrose@aldebaran.com>
2016-08-11 14:13:04 +0200

    Check if an annotation has coordinates before trying to use them

--- qidata_gui/apps/annotator/controller/controllers/image_controller.py
+++ qidata_gui/apps/annotator/controller/controllers/image_controller.py
@@ -122,18 +122,23 @@ class ImageController(DataController):
         self.widget.displayObject(self.local_model[self.last_selected_item_index][0])
 
     def createNewItem(self, center_coordinates):
-        x=center_coordinates[0]
-        y=center_coordinates[1]
+        if center_coordinates is not None:
+            x=center_coordinates[0]
+            y=center_coordinates[1]
 
-        # Create the new object as required
-        new_object = [makeMetadataObject(self.last_selected_item_type),[[x-30,y-30],[x+30,y+30]]]
+            # Create the new object as required
+            new_object = [makeMetadataObject(self.last_selected_item_type),[[x-30,y-30],[x+30,y+30]]]
+
+        else:
+            # Create the new object as required
+            new_object = [makeMetadataObject(self.last_selected_item_type),None]
 
         # Add it to local and general model
         self.local_model.append(new_object)
         self.model[str(self.last_selected_item_type)].append(new_object)
 
         # Display it in the image widget
-        r = self.addAnnotationItemOnView(self.local_model[-1][1])
+        r = self.addAnnotationItemOnView(new_object[1])
 
         # Display information on it in data editor widget
         r.select()
