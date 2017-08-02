35740e2a3f31cca1b84b0732b4e9438665fd1714
sambrose <sambrose@aldebaran.com>
2017-08-02 18:40:59 +0200

    qidata_gui package basic structure

--- qidata_gui/__init__.py
+++ qidata_gui/__init__.py
@@ -8,19 +8,15 @@ This package contains all widgets necessary to display data and any metadata lin
 It also contains several graphical applications using those widgets.
 """
 
-import os, glob
-
-# ––––––––––––––––––
-# Export all modules
-
-__all__ = [os.path.basename(f)[:-3] for f in glob.glob(os.path.dirname(__file__)+"/*.py")]
-
-try: del f # Cleanup the iteration variable so that it's not exported
-except: pass
+# Standard libraries
+import os as _os
 
 # ––––––––––––––––––––––––––––
 # Convenience version variable
 
-VERSION = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "VERSION")).read().split()[0]
+VERSION = open(_os.path.join(_os.path.dirname(_os.path.realpath(__file__)),
+                             "VERSION")
+              ).read().split()[0]
+
 
 #––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––#
--- setup.py
+++ setup.py
@@ -6,21 +6,38 @@ import os
 
 CONTAINING_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
+try:
+    from utils import get_version_from_tag
+    __version__ = get_version_from_tag()
+    open(os.path.join(CONTAINING_DIRECTORY,"qidata_gui/VERSION"), "w").write(__version__)
+except ImportError:
+    __version__=open(os.path.join(CONTAINING_DIRECTORY,"qidata_gui/VERSION")).read().split()[0]
+
+package_list = find_packages(where=os.path.join(CONTAINING_DIRECTORY))
+
 setup(
     name='qidata_gui',
-    version=open(os.path.join(CONTAINING_DIRECTORY,"qidata_gui/VERSION")).read().split()[0],
+    version=__version__,
+    description='Graphical interface for qidata suite',
+    long_description=open(os.path.join(CONTAINING_DIRECTORY,'README.md')).read(),
+    url='https://gitlab.aldebaran.lan/qidata/qidata_gui',
     author='Surya Ambrose <sambrose@aldebaran.com>, Louis-Kenzo Cahier <lkcahier@aldebaran.com>',
     author_email='sambrose@aldebaran.com',
-    packages=find_packages("."),
-    package_data={"qidata_gui":["VERSION"]},
-    url='.',
     license='LICENSE.txt',
-    description='Contains Qt Widgets to display metadata objects and interact with it, and also provide Qt Apps',
-    long_description=open(os.path.join(CONTAINING_DIRECTORY,'README.md')).read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='metadata annotation tagging',
+    packages=package_list,
     install_requires=[
         "PySide >= 1.2.2",
-        "qidata >= 0.3.0",
-        "argcomplete >= 1.1.0"
-    ]
+        "qidata == 1.0.0a3",
+        "argcomplete >= 1.1.0",
+    ],
+    package_data={"qidata_gui":["VERSION"]},
 )
 
