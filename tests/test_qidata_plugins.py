6342bbd6ce7b53c2af493fe2535a9092ab143c00
sambrose <sambrose@aldebaran.com>
2017-08-11 17:54:44 +0200

    Rework the commands tests
    Also, change some stuff on tox and tests/ so that files are directly used
    instead of the ones installed in tox

--- tests/test_qidata_plugins.py
+++ tests/test_qidata_plugins.py
@@ -3,20 +3,30 @@
 import subprocess
 import time
 
-def test_qidata_open_command(jpg_file_path):
-	p=subprocess.Popen(["qidata",
-	                    "open",
-	                    jpg_file_path])
-	time.sleep(1)
-	p.terminate()
-
-def test_qidata_open_app(jpg_file_path):
+def test_qidata_commands(jpg_file_path):
+	# Make sure the commands are really defined upon installation
 	subprocess.check_call(["qidata",
 	                       "open",
-	                       jpg_file_path])
+	                       "-h"])
 
-def test_qidata_annotate_app(dataset_path):
 	subprocess.check_call(["qidata",
 	                       "annotate",
+	                       "-h"])
+
+	subprocess.check_call(["qidata",
+	                       "extract",
+	                       "-h"])
+
+def test_open_app(jpg_file_path):
+	subprocess.check_call(["python",
+	                       "-m",
+	                       "qidata_apps.viewer.app",
+	                       jpg_file_path])
+
+def test_annotate_app(dataset_path):
+	subprocess.check_call(["python",
+	                       "-m",
+	                       "qidata_apps.annotator.app",
 	                       "sambrose",
 	                       dataset_path])
+
